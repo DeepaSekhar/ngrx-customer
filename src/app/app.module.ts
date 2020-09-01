efd88977b91f13808b62dcc217ae553fd2f6cf6a
deepaakhil84 <deepaakhiltvm@gmail.com>
2020-09-01 14:43:39 +0100

    basic component creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store'
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CustomersComponent } from './customers/customers.component';
+import { CustomerComponent } from './customers/customer/customer.component';
+import { CustomerAddComponent } from './customers/customer-add/customer-add.component';
+import { CustomerEditComponent } from './customers/customer-edit/customer-edit.component';
+import { CustomerListComponent } from './customers/customer-list/customer-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    CustomersComponent,
+    CustomerComponent,
+    CustomerAddComponent,
+    CustomerEditComponent,
+    CustomerListComponent
   ],
   imports: [
     BrowserModule,
